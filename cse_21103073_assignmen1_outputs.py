0f6e3095528b7136cc2998748e24637638f33e91
DAKSH2004 <97343888+DAKSH2004@users.noreply.github.com>
2022-01-08 18:03:53 +0530

    Add files via upload

---
+++ cse_21103073_assignmen1_outputs.py
@@ -0,0 +1,47 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================== RESTART: C:\python.py\q1.py =========================
+enter the value of num1: 23
+enter the value of num2: 34
+enter the value of num3: 45
+the average is:  34.0
+
+========================== RESTART: C:\python.py\q2.py =========================
+enter the gross income: 100000
+enter the no. of dependents: 5
+taxable income:  75000.0
+tax:  15000.0
+
+========================== RESTART: C:\python.py\q3.py =========================
+Enter student id: 21103073
+Enter name: DAKSH KHANDELWAL
+Enter gender: M
+Enter the name of the course: CSE
+Enter the cgpa: 9.2
+['SID', 'Name', 'Gender', 'Course Name', 'CGPA']
+[21103073, 'DAKSH KHANDELWAL', 'M', 'CSE', 9.2]
+
+========================== RESTART: C:\python.py\q4.py =========================
+Enter the student 1 marks: 45
+Enter the student 2 marks: 56
+Enter the student 3 marks: 12
+Enter the student 4 marks: 91
+Enter the student 5 marks: 72
+list: 
+[45, 56, 12, 91, 72]
+sorted list: 
+[12, 45, 56, 72, 91]
+
+======================== RESTART: C:\python.py\q5(a).py ========================
+list: 
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+The 4th term is:  Black
+After removing the 4th term new list: 
+['Red', 'Green', 'White', 'Pink', 'Yellow']
+
+======================== RESTART: C:\python.py\q5(b).py ========================
+list: 
+['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']
+new list after removing and replacing: 
+['Red', 'Green', 'White', 'Purple', 'Yellow']
