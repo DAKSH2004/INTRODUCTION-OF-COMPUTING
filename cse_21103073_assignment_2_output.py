5858e7665495b1ee6f1f184bf52e6d6557c2baff
DAKSH2004 <97343888+DAKSH2004@users.noreply.github.com>
2022-01-23 03:40:18 +0530

    Add files via upload

---
+++ cse_21103073_assignment_2_output.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================= RESTART: C:\python.py\a2q1.py ========================
+A:
+length of the string: 35
+B
+Reverse of the string is: ' egaugnal evitisnes esac a si nohtyP '
+C
+new string is: '  a case sensitive '
+D
+Python is object oriented language
+E
+Index of 'a' in the given string is: 10
+F
+Pythonisacasesensitivelanguage
+
+========================= RESTART: C:\python.py\a2q2.py ========================
+Enter your name:DAKSH KHANDELWAL
+enter your SID:21103073
+Enter your Department Name:CSE
+Enter your CGPA:9.9
+Hey, DAKSH KHANDELWAL here!
+My SID is 21103073 
+I am from  CSE Department and my CGPA is 9.9
+
+========================= RESTART: C:\python.py\a2q3.py ========================
+A
+a&b: 8
+B
+a|b: 58
+C
+a^b: 50
+D
+left shift a by 2: 224 
+left shift b by 2: 40
+E
+right shift a by 2: 14 
+right shift b by 4: 0
+
+========================= RESTART: C:\python.py\a2q4.py ========================
+Enter first number: 123.7
+Enter second number: 123.5
+Enter third number: 123.9
+The largest number is: 123.9
+
+========================= RESTART: C:\python.py\a2q5.py ========================
+enter the string:hi,my name is daksh
+Yes
+
+========================= RESTART: C:\python.py\a2q5.py ========================
+enter the string:hi,daksh
+No
+
+========================= RESTART: C:\python.py\a2q6.py ========================
+Enter the value of side a:3
+Enter the value of side b:4
+Enter the value of side c:5
+Yes
+
+========================= RESTART: C:\python.py\a2q6.py ========================
+Enter the value of side a:3 
+Enter the value of side b:3
+Enter the value of side c:7
+No
