3fd055eccde810f518af18c480acd88ee00e2168
DAKSH2004 <97343888+DAKSH2004@users.noreply.github.com>
2022-02-14 13:34:09 +0530

    Add files via upload

---
+++ cse_21103073_assignment_3_output_1.py
@@ -0,0 +1,101 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================= RESTART: C:\python.py\a3q1.py ========================
+Q1
+enter the string:mother
+{'m': 1, 'o': 1, 't': 1, 'h': 1, 'e': 1, 'r': 1}
+
+========================= RESTART: C:\python.py\a3q2.py ========================
+Q2
+ENTER THE Day: 14593
+Please Enter a valid Day
+ENTER THE Day: 12
+ENTER THE MONTH OF THE YEAR: 1334
+Please Enter a valid Month
+ENTER THE MONTH OF THE YEAR: 12
+ENTER THE YEAR: 1234
+Please Enter Year from 1800 to 2025 only
+ENTER THE YEAR: 1999
+13 / 12 / 1999
+
+========================= RESTART: C:\python.py\a3q3.py ========================
+Q3
+Enter elements of list seperated by spaces: 1 3 5 7 13 19
+list:  ['1', '3', '5', '7', '13', '19']
+The Resultant tuple is:
+[(1, 1), (3, 9), (5, 25), (7, 49), (13, 169), (19, 361)]
+
+========================= RESTART: C:\python.py\a3q4.py ========================
+Q4
+Enter the grade points: 9
+your grade is 'A' and Excellent performance
+
+========================= RESTART: C:\python.py\a3q5.py ========================
+Q5
+no of times pattern to be formed:6
+ABCDEFGHIJK
+ ABCDEFGHI
+  ABCDEFG
+   ABCDE
+    ABC
+     A
+
+========================= RESTART: C:\python.py\a3q6.py ========================
+Q6
+Enter SID: 1234
+Enter Name: raj
+Do you want to enter another student entry(Y or N):y
+Enter SID: 1235
+Enter Name: sonuja
+Do you want to enter another student entry(Y or N):y
+Enter SID: 1236
+Enter Name: rajesh
+Do you want to enter another student entry(Y or N):y
+Enter SID: 1237
+Enter Name: abcd
+Do you want to enter another student entry(Y or N):n
+A)
+{1234: 'raj', 1235: 'sonuja', 1236: 'rajesh', 1237: 'abcd'}
+B)
+[(1237, 'abcd'), (1234, 'raj'), (1236, 'rajesh'), (1235, 'sonuja')]
+c)
+{1234: 'raj', 1235: 'sonuja', 1236: 'rajesh', 1237: 'abcd'}
+D)
+Search Name with SID: 1236
+rajesh
+
+========================= RESTART: C:\python.py\a3q7.py ========================
+Q7
+ENTER THE NUMBER OF TERMS IN THE SERIES: 16
+Fibonacci sequence:
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+144
+233
+377
+610
+AVERAGE OF RESULTANT FIBONACCI SERIES IS 99.75
+
+========================= RESTART: C:\python.py\a3q8.py ========================
+Q8
+A)
+{1, 3, 5, 6, 8}
+B)
+{3, 6, 8, 9, 13, 17}
+C)
+{1, 2, 4, 5}
+D)
+{6, 7, 8, 9, 10}
+E)
+{10, 7}
