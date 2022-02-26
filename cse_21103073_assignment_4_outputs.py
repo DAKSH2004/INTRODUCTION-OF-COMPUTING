309849278fda842f2d86479be2b2f421b0f79f9e
DAKSH2004 <97343888+DAKSH2004@users.noreply.github.com>
2022-02-27 02:10:11 +0530

    Add files via upload

---
+++ cse_21103073_assignment_4_outputs.py
@@ -0,0 +1,65 @@
+Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+=========== RESTART: C:\python.py\cse_21103073_assignment_4_CODES.py ===========
+Q1:
+move disk 1 from a to destination b
+move disk 2 from a to destination b
+move disk 1 from c to destination b
+move disk 3 from a to destination b
+move disk 1 from c to destination b
+move disk 2 from c to destination b
+move disk 1 from a to destination b
+
+Q2:
+enter the no of rows: 6
+using loops
+       1 
+      1 1 
+     1 2 1 
+    1 3 3 1 
+   1 4 6 4 1 
+  1 5 10 10 5 1 
+using recurssion
+          1   
+        1   1   
+      1   2   1   
+    1   3   3   1   
+  1   4   6   4   1   
+1   5   10   10   5   1   
+
+Q3:
+enter the 1st number: 4
+enter the 2nd number: 3
+a)
+checking caliability
+False
+False
+b)
+both remainder and quotient are non zero
+c)
+number that are greator than 4 int the list is: [5, 5, 6, 6, 7, 7] 
+d)
+{5, 6, 7}
+e)
+immutable set is:  frozenset({5, 6, 7})
+f)
+maximum value:  7
+hash value of the max value of the above set is:  7
+
+Q4:
+object is created
+students's name rakesh and sid is 1234.
+object is destroyed
+
+Q5:
+a)
+updated salary of mehak is 70000.
+b)
+record of viren deleted
+
+Q6:
+enter a word: peek
+enter a new word which has meaning using the the same letter to test friendship: keep
+check whether the formed word have a meaning(y/Y or n/N):y
+friendship is real and friend passes the test
